927c9d8b56bdfc7ad5565a2958182dc19dcd11e8
OjasRasam <ojasrasam@gmail.com>
2025-01-14 18:33:49 +0530

    Edited some settings for hosting

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@ document.getElementById("message-form").addEventListener("submit", async (e) => 
    e.preventDefault();
    const message = document.getElementById("message").value;
 
-   const response = await fetch("https://storied-cannoli-9dde5c.netlify.app//message", {
+   const response = await fetch("https://storied-cannoli-9dde5c.netlify.app/message", {
        method: "POST",
        headers: { "Content-Type": "application/json" },
        body: JSON.stringify({ message }),
